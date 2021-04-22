0b8ac5ad4f0fffbc905d254f51aef0acf3410273
mehedi23 <satusatusatu0@gmail.com>
2021-04-22 10:05:09 +0600

    small react-redux with counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useSelector , useDispatch} from 'react-redux'
 
 function App() {
+
+  const dispatching = useDispatch();
+  const state = useSelector(state => state.counter);
+
+  console.log(state)
+
+  const incrementCall = () =>{
+    dispatching({ type: "increment" })
+  }
+
+  const dicrementCall = () => {
+    dispatching({type: "dicrement"})
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>{state}</h1>
+
+       <button onClick={incrementCall}>+</button>
+       <button onClick={dicrementCall}>-</button>
     </div>
   );
 }
